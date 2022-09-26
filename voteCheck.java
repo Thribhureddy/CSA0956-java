3b51598513b96961393e464c4d010672d813e7fb
Thribhureddy <112462824+Thribhureddy@users.noreply.github.com>
2022-09-26 09:47:43 +0530

    Add files via upload

---
+++ voteCheck.java
@@ -0,0 +1,15 @@
+import java.io.*; 
+import java.util.*; 
+class voteCheck 
+{ 
+        public static void main(String args[]) 
+        { 
+             Scanner sc=new Scanner(System.in); 
+             System.out.println("Enter the age:"); 
+             int age=sc.nextInt(); 
+             if(age>=18) 
+                   System.out.println("You are eligible for voting"); 
+             else  
+                   System.out.println("You are not eligible for voting"); 
+        } 
+}
