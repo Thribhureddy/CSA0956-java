b19286945970a46bdc9c83571e51773555a7da39
Thribhureddy <112462824+Thribhureddy@users.noreply.github.com>
2022-10-31 09:11:17 +0530

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,13 @@
+
+public class stringlength{
+	public static void main(String[] args)
+	{
+		
+		String str = "GeeksforGeeks";
+
+		System.out.println(
+			"The size of "
+			+ "the String is "
+			+ str.length());
+	}
+}
